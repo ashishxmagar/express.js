a2824143616eb6de90b09e07f03613848d7c5ae3
Ashish T Magar <aasizmagar10@gmail.com>
2025-05-08 21:56:15 +0200

    tried res.json

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.get('/query', (req, res, next) => {
+  console.log('Response 1');
+  next('route');
+})
 
-app.get('/query', (req, res) => {
-  console.log(req.query.name)
-  res.send('Hello World');
+app.get('/query', (req, res, next) => {
+  console.log('Response 2');
+  res.json({name: 'Ashish', surname:'Thapa Magar', profession:'Developer'});
 })
 
 
 app.listen(3000, () => {
-  console.log('Server listening at port 3000....');
+  console.log('Server listening on port 3000.....');
 })
