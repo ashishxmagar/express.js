90e8ff15c5915b5bbda9e7bddd857d84e3068cf8
Ashish T Magar <aasizmagar10@gmail.com>
2025-05-11 21:45:06 +0200

    added all files

---
+++ logger.js
@@ -0,0 +1,9 @@
+const person  = {
+  name: 'Ashish',
+  surname:'Thapa Magar',
+  profession: 'Developer'
+}
+
+
+const {name, surname, profession} = person;
+console.log(name, surname, profession);
